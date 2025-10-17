462256939a0e35b191ed7692d77a433ed187d728
herbertschrotter-blip <herbert.schrotter@gmail.com>
2025-10-17 19:12:27 +0200

    Startdatei für cmd.exe

---
+++ Start_MasterSetup.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo =============================================
+echo   🧭 Starte Master Setup Framework
+echo =============================================
+pwsh.exe -NoLogo -ExecutionPolicy Bypass -File "%~dp0Master_Controller.ps1"
+pause
